3b56be312ab6adbefe1f4f0dee2fed881543bb36
xiaoniao <liuzhuang@outlook.com>
2018-01-03 15:58:03 +0800

    jedis基本用法

--- jedis-sample/src/main/java/com/example/jedissample/JedisSampleApplication.java
+++ jedis-sample/src/main/java/com/example/jedissample/JedisSampleApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JedisSampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JedisSampleApplication.class, args);
+		 SpringApplication.run(JedisSampleApplication.class, args);
 	}
 }
