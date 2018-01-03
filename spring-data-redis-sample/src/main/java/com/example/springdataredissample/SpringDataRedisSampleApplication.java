796f44079b4db02b35aa14c861ad35e61e82023b
xiaoniao <liuzhuang@outlook.com>
2018-01-03 15:24:01 +0800

    初始化项目

---
+++ spring-data-redis-sample/src/main/java/com/example/springdataredissample/SpringDataRedisSampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.springdataredissample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRedisSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRedisSampleApplication.class, args);
+	}
+}
