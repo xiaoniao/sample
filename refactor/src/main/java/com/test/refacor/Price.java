0188a7f958cacbea89171123694820d348c2f5e8
xiaoniao <liuzhuang@outlook.com>
2018-01-30 16:41:24 +0800

    first

--- refactor/src/main/java/com/test/refacor/Price.java
+++ refactor/src/main/java/com/test/refacor/Price.java
@@ -5,6 +5,41 @@ package com.test.refacor;
  */
 public class Price {
 
-//    public
+    /**
+     * 数量
+     */
+    private int quantity;
+
+    /**
+     * 单价
+     */
+    private double itemPrice;
+
+    public double getPrice() {
+        double basePrice = quantity * itemPrice;
+
+        int discountLevel;
+        if (quantity > 100) {
+            discountLevel = 2;
+        } else {
+            discountLevel = 1;
+        }
+        double finalPrice = discountedPrice(basePrice, discountLevel);
+        return finalPrice;
+    }
+
+    private double discountedPrice(double basePrice, int discountLevel) {
+        if (discountLevel == 2) {
+            return basePrice * 0.1;
+        }
+        return basePrice * 0.05;
+    }
+
+    public static void main(String[] args) {
+        Price price = new Price();
+        price.quantity = 10;
+        price.itemPrice = 5;
+        System.out.println(price.getPrice());
+    }
 
 }
