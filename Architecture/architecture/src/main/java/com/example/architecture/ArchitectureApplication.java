e82ccfecaeb45f7daea4d68f8774ebae89ae091f
xiaoniao <liuzhuang@outlook.com>
2018-03-19 15:22:53 +0800

    架构

---
+++ Architecture/architecture/src/main/java/com/example/architecture/ArchitectureApplication.java
@@ -0,0 +1,12 @@
+package com.example.architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectureApplication.class, args);
+	}
+}
---
+++ Architecture/architecture/src/main/java/com/example/architecture/domain/UserDO.java
@@ -0,0 +1,57 @@
+package com.example.architecture.domain;
+
+/**
+ * Created by liuzz on 2018/03/19
+ */
+public class UserDO {
+
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private String location;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", location='" + location + '\'' +
+                '}';
+    }
+}
