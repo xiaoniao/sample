5e1fa0787fd4191d6c0f8be7f74acbe0b418b04f
xiaoniao <liuzhuang@outlook.com>
2018-05-10 17:09:03 +0800

    spring-retry

---
+++ SPRING/spring-retry-learn/src/main/java/com/example/springretrylearn/SpringRetryLearnApplication.java
@@ -0,0 +1,12 @@
+package com.example.springretrylearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRetryLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRetryLearnApplication.class, args);
+	}
+}
