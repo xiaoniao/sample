a9f811e83a2733863b599f6e6a5777d58496f817
xiaoniao <liuzhuang@outlook.com>
2018-05-17 16:57:47 +0800

    init rocketmq rabbitmq

---
+++ MQ/RabbitMQ/robbitmq-learn/src/main/java/com/example/robbitmqlearn/RobbitmqLearnApplication.java
@@ -0,0 +1,12 @@
+package com.example.robbitmqlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobbitmqLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobbitmqLearnApplication.class, args);
+	}
+}
---
+++ MQ/RocketMQ/rocketmq-learn/src/main/java/com/example/rocketmqlearn/RocketmqLearnApplication.java
@@ -0,0 +1,12 @@
+package com.example.rocketmqlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketmqLearnApplication.class, args);
+	}
+}
