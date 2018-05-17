94d86d9c907093ac37beeac8d6cad6c89f17c232
xiaoniao <liuzhuang@outlook.com>
2018-05-17 17:22:22 +0800

    spark

---
+++ Spark/spark-learn/src/test/java/com/example/sparklearn/SparkLearnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.sparklearn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SparkLearnApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
